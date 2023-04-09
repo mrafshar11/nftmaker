2441bd8763d132070b984dfda8d054bd3813d5a9
mrafshar11 <mohamadreza11afshar@gmail.com>
2023-04-10 02:35:17 +0330

    edit 19

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from "next-auth/providers/Apple";
+import AppleProvider from "next-auth/providers/apple"
 
 export default NextAuth({
   secret: process.env.SECRET,
